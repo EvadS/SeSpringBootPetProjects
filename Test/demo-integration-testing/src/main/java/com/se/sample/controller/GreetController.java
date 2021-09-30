3801854c716a7d124dc5a07290d64248afac855a
ski.evgeniy <ski.evgeniy@gmail.com>
2021-09-30 19:20:09 +0300

    started work with integration testing

---
+++ Test/demo-integration-testing/src/main/java/com/se/sample/controller/GreetController.java
@@ -0,0 +1,20 @@
+package com.se.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/greeting")
+public class GreetController {
+
+    @GetMapping
+    public String index() {
+        return "hello";
+
+        try {
+
+        }catch() {
+
+        }
+
+    }
+}
