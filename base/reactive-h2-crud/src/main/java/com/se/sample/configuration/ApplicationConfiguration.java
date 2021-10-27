a5ff4c72ec32c0e4c328f3abf5a3d76e22ee853c
ski.evgeniy <ski.evgeniy@gmail.com>
2021-10-27 14:34:06 +0300

    removed deprecated code

--- base/reactive-h2-crud/src/main/java/com/se/sample/configuration/ApplicationConfiguration.java
+++ base/reactive-h2-crud/src/main/java/com/se/sample/configuration/ApplicationConfiguration.java
@@ -5,29 +5,28 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 
 /**
- *  Class to initialize any configurations or beans needed for application
+ * Class to initialize any configurations or beans needed for application
  */
 @Configuration
 public class ApplicationConfiguration {
+
     @Bean
     public ConnectionFactoryInitializer databaseInitializer(ConnectionFactory connectionFactory) {
 
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
 
-        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        org.springframework.r2dbc.connection.init.CompositeDatabasePopulator populator = new org.springframework.r2dbc.connection.init.CompositeDatabasePopulator();
         populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema/schema.sql")));
         populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema/data.sql")));
+
         initializer.setDatabasePopulator(populator);
 
         return initializer;
     }
-
-
 }
