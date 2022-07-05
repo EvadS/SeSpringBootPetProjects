427fbb784aef06249a0cf234e688f7accce31437
Evgeniy Skiba <evad.se.dev@gmail.com>
2022-07-05 18:46:41 +0300

    custom metrics

--- metrics/demo-prometeus/Readme.md
+++ metrics/demo-prometeus/Readme.md
@@ -1,14 +1,27 @@
 
 
-```
-    docker run -p 9090:9090 -v /home/softkit/Documents/projects/Learning/SeSpringBootPetProjects/metrics/demo-prometeus/src/main/resources/prometheus.yml prom/prometheus
+http://localhost:8081/actuator/prometheus
 
+
+```
+    docker run -p 9090:9090  --name=my-prometheus -v /home/softkit/Documents/projects/Learning/SeSpringBootPetProjects/metrics/demo-prometeus/src/main/resources/prometheus.yml prom/prometheus
+```
+
+
+
+```
+    docker run -d -p 9090:9090 \
+    --name=my-prometheus \
+     -v /home/softkit/Documents/projects/Learning/SeSpringBootPetProjects/metrics/demo-prometeus/src/main/resources/prometheus.yml:/etc/prometheus/prometheus.yml \
+     prom/prometheus
 ```
 
 prometheus
 ```
     http://localhost:9090
 ```
+
+Stautus-> target
 
 graphana 
 ```bash
@@ -21,3 +34,26 @@ sudo ufw allow from 172.18.0.0/24
     http://localhost:3000
 ```
 admin/admin
+-----
+
+## Demonstrates how to send custom metrics using micrometer
+```
+    http://localhost:8081/actuator/health
+```
+
+
+http://localhost:8080/actuator/prometheus
+
+http GET "http://localhost:8080/actuator/prometheus" | grep custom
+
+
+custom metric named custom_gauge
+
+
+https://grafana.com/grafana/dashboards/4701
+
+------
+дополнительно
+https://github.com/Kirya522/medium-posts/tree/main/java/spring-metrics/src/metricsdemo/scripts
+---
+https://mokkapps.de/blog/monitoring-spring-boot-application-with-micrometer-prometheus-and-grafana-using-custom-metrics/
---
+++ metrics/demo-prometeus/src/main/java/com/se/sample/components/Scheduler.java
@@ -0,0 +1,51 @@
+package com.se.sample.components;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.prometheus.client.Gauge;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Scheduler {
+
+    private final AtomicInteger testGauge;
+    private final Counter testCounter;
+
+    private Gauge guage;
+
+    public Scheduler(MeterRegistry meterRegistry) {
+        // Counter vs. gauge, summary vs. histogram
+        // https://prometheus.io/docs/practices/instrumentation/#counter-vs-gauge-summary-vs-histogram
+        testGauge = meterRegistry.gauge("custom_gauge", new AtomicInteger(0));
+        testCounter = meterRegistry.counter("custom_counter");
+
+        Gauge.build()
+
+
+    }
+
+    @Scheduled(fixedRateString = "10000", initialDelayString = "0")
+    public void schedulingTask() {
+        int randomNumberInRange = Scheduler.getRandomNumberInRange(0, 100);
+
+        log.info("scheduler value:{}", randomNumberInRange);
+        testGauge.set(randomNumberInRange);
+
+        testCounter.increment();
+    }
+
+    private static int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+}
